ee0db0b6346c1f2d5e3924e309e9ce0d3a671dd3
emperatorLeo <leo_svxy@hotmail.com>
2025-05-14 17:42:16 -0500

    adding retrofit,Okhttp,Gson and Hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt.kapt)
+    alias(libs.plugins.ksp.plugin)
 }
 
 android {
@@ -49,6 +51,23 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Retrofit
+    api(libs.retrofit)
+    api(libs.gson.converter)
+
+    // Okhttp
+    api(libs.okhttp)
+    api(libs.okhttp.logging.interceptor)
+
+    // Gson
+    api(libs.gson)
+
+    // Hilt
+    api(libs.hilt)
+    api(libs.hilt.navigation)
+    kapt(libs.hilt.kapt)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +75,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+kapt {
+    correctErrorTypes = true
 }
